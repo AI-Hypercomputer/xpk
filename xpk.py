f1517bafdf2ea30948314c7649578f840221776f
Surbhi Jain <sjsurbhi@google.com>
2024-04-01 14:40:14 -0700

    Add dynamic versioning for pip package (#105)
    * Add dynamic versioning for pip package
    
    * Add a new line
    
    * Add comment for version

--- xpk.py
+++ xpk.py
@@ -66,7 +66,9 @@ if (
 default_docker_image = 'python:3.10'
 default_script_dir = os.getcwd()
 default_gke_version="1.29.1-gke.1589017"
-xpk_current_version = "0.3.0"
+# This is the version for XPK PyPI package
+__version__ = "0.3.0"
+xpk_current_version = __version__
 _CLUSTER_QUEUE_NAME='cluster-queue'
 _LOCAL_QUEUE_NAME='multislice-queue'
 h100_device_type = 'h100-80gb-8'

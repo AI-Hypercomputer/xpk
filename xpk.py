acdbe2d4877f8fb9035663bc4ea2181d7c90f41e
Kyle Sorensen <kylesorensen@google.com>
2024-04-19 16:42:02 -0600

    Update xpk.py
    update default version for gke

--- xpk.py
+++ xpk.py
@@ -67,7 +67,7 @@ if (
 
 default_docker_image = 'python:3.10'
 default_script_dir = os.getcwd()
-default_gke_version = '1.29.1-gke.1589017'
+default_gke_version = '1.29.3-gke.1093000'
 # This is the version for XPK PyPI package
 __version__ = '0.4.0'
 xpk_current_version = __version__

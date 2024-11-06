3889bfe54a7e5b9a9ffbfc38b1777158105baa02
Piotr Pawłowski <ppawl@google.com>
2024-11-06 11:01:43 +0000

    check pyright
    Signed-off-by: Piotr Pawłowski <ppawl@google.com>

---
+++ src/xpk/core/docker_manager.py
@@ -0,0 +1,20 @@
+
+
+class GclusterDockerManager:
+  def __init__(self, vol_path: str) -> None:
+    pass
+
+  def verify_docker_installed(self):
+    pass
+
+  def build_image(self, img_name: str, docker_file_path: str):
+    pass
+
+  def run_container(self, container_name: str):
+    pass
+
+  def upload_file(self, file_path: str):
+    pass
+
+  def upload_dir(self, dir_path: str):
+    pass

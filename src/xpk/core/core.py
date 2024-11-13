fd68b25c0f88f615fb52075cf7bac2272675d41d
Sujeeth Jinesh <sujinesh@google.com>
2024-11-13 19:10:52 +0000

    Replace Cloud Platform URL with Global Constant

--- src/xpk/core/core.py
+++ src/xpk/core/core.py
@@ -77,6 +77,7 @@ DEFAULT_VERTEX_TENSORBOARD_NAME = 'tb-instance'
 AUTOPROVISIONING_CONFIG_VALUE = 'AUTOPROVISION'
 AUTOPROVISIONING_CONFIG_MINIMUM_KEY = 'minimum_chips'
 AUTOPROVISIONING_CONFIG_MAXIMUM_KEY = 'maximum_chips'
+CLOUD_PLATFORM_AUTH_SCOPE_URL = '"https://www.googleapis.com/auth/cloud-platform"'
 
 
 class CapacityType(enum.Enum):
@@ -1275,7 +1276,7 @@ def run_gke_node_pool_create_command(
       command += f' --num-nodes={system.vms_per_slice}'
       command += ' --placement-type=COMPACT  --max-pods-per-node 15'
       command += (
-          ' --scopes=storage-full,gke-default,"https://www.googleapis.com/auth/cloud-platform"'
+          f' --scopes=storage-full,gke-default,{CLOUD_PLATFORM_AUTH_SCOPE_URL}'
       )
       command += f' --tpu-topology={system.topology}'
       command += f' {args.custom_tpu_nodepool_arguments}'
@@ -1286,7 +1287,7 @@ def run_gke_node_pool_create_command(
           ' --accelerator'
           f' type={system.gke_accelerator},count={str(system.chips_per_vm)},gpu-driver-version=latest'
           ' --no-enable-autoupgrade '
-          ' --scopes="https://www.googleapis.com/auth/cloud-platform"'
+          f' --scopes={CLOUD_PLATFORM_AUTH_SCOPE_URL}'
           ' --additional-node-network'
           f' network={args.cluster}-net-1,subnetwork={subnet_prefix}-sub-1'
           ' --additional-node-network'
@@ -1325,7 +1326,7 @@ def run_gke_node_pool_create_command(
       command = (
           'gcloud beta container node-pools create'
           f' {node_pool_name} --node-version={gke_node_pool_version} --cluster={args.cluster} --project={args.project} --node-locations={args.zone} --region={zone_to_region(args.zone)} --num-nodes=1'
-          f' --machine-type={args.pathways_gce_machine_type} --scopes=storage-full,gke-default,"https://www.googleapis.com/auth/cloud-platform"'
+          f' --machine-type={args.pathways_gce_machine_type} --scopes=storage-full,gke-default,{CLOUD_PLATFORM_AUTH_SCOPE_URL}'
           ' --enable-autoscaling --min-nodes=1 --max-nodes=20'
       )
       task = f'NodepoolCreate-{node_pool_name}'

544e03c2c53cb581d4b8eea8ce8f46116ef85755
Piotr Pawłowski <ppawl@google.com>
2024-11-14 12:04:15 +0000

    linters fix
    Signed-off-by: Piotr Pawłowski <ppawl@google.com>

--- src/xpk/core/tests/test_blueprint.py
+++ src/xpk/core/tests/test_blueprint.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2024 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from ..blueprint import create_a3_mega_blueprint
 import ruamel.yaml
 
@@ -18,7 +34,7 @@ def test_create_a3_mega_blueprint():
       zone="us-central1-c",
       auth_cidr="10.0.0.0/32",
   )
-  with open(a3_yaml_test_path) as stream:
+  with open(a3_yaml_test_path, encoding="utf-8") as stream:
     ctk_yaml = yaml.load(stream)
     assert ctk_yaml.blueprint_name == ctk_test.blueprint_name
     assert ctk_yaml.vars == ctk_test.vars

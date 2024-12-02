5b821da256b70289d42ceecbc81b277d10e7feb9
Piotr Pawłowski <ppawl@google.com>
2024-12-02 10:49:46 +0000

    fix formatting
    Signed-off-by: Piotr Pawłowski <ppawl@google.com>

--- src/xpk/core/docker_manager.py
+++ src/xpk/core/docker_manager.py
@@ -165,7 +165,10 @@ class CtkDockerManager(CtkCommandRunner):
       - docker.errors.APIError
     """
     xpk_print(f"Running command: {cmd} inside container: {self.container_name}")
-    xpk_print(f'volumes: {self.gcloud_cfg_path}:{gcloud_cfg_mount_path}, {self.deployment_dir}:{deployment_dir_mount_path}')
+    xpk_print(
+        f"volumes: {self.gcloud_cfg_path}:{gcloud_cfg_mount_path},"
+        f" {self.deployment_dir}:{deployment_dir_mount_path}"
+    )
     try:
       self.client.containers.run(
           image=self.img_name,

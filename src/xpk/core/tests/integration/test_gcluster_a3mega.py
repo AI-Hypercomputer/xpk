a7cc9aeb73ed71c45a12da7871443deca9475323
Piotr Pawłowski <ppawl@google.com>
2024-12-09 08:48:08 +0000

    uncomment tests
    Signed-off-by: Piotr Pawłowski <ppawl@google.com>

--- src/xpk/core/tests/integration/test_gcluster_a3mega.py
+++ src/xpk/core/tests/integration/test_gcluster_a3mega.py
@@ -29,12 +29,12 @@ auth_cidr = os.getenv("AUTH_CIDR")
 deployment_dir = os.getenv("DEPLOYMENT_DIR")
 
 
-@pytest.mark.skip(
-    reason=(
-        "This test requires A3 capacity, therefore it should not be run on each"
-        " build. Please invoke it manually if needed. "
-    )
-)
+# @pytest.mark.skip(
+#     reason=(
+#         "This test requires A3 capacity, therefore it should not be run on each"
+#         " build. Please invoke it manually if needed. "
+#     )
+# )
 def test_create_ctk_deployment():
   assert project_id is not None
   assert deployment_name is not None
@@ -78,4 +78,4 @@ def test_create_ctk_deployment():
   assert os.path.exists(os.path.join(deployment_type_dir, deployment_name))
 
 
-#   ctk_manager.destroy_deployment()
+  ctk_manager.destroy_deployment()

17433ad2355b8fcd7dc87ae889c74fc0c3bde05a
RoshaniN <roshanin@google.com>
2024-12-13 18:14:26 +0000

    XLA flags added to McJAX LR workload added for parity.

--- src/xpk/core/pathways.py
+++ src/xpk/core/pathways.py
@@ -45,7 +45,8 @@ def get_pathways_worker_args(args) -> str:
               - --temporary_flags_for_debugging=temporary_flag_for_debugging_megascale_address_derive_from_megascale_grpc=true
               - --megascale_grpc_premap_memory_bytes=17179869184
               - --gcs_scratch_location={args.pathways_gcs_location}
-              - --megascale_graph_within_launch_hang_threshold=5m"""  # More flags we can adjust here: https://source.corp.google.com/piper///depot/google3/platforms/xla/megascale/runtime/executor/executor.cc;l=53-81;rcl=705575633
+              - --megascale_graph_within_launch_hang_threshold=5m
+              - --deepsea_chip_config_name=megachip_tccontrol"""  # More flags we can adjust here: https://source.corp.google.com/piper///depot/google3/platforms/xla/megascale/runtime/executor/executor.cc;l=53-81;rcl=705575633
 
   if args.use_pathways:
     return yaml.format(args=args, rm_address=get_rm_address(args))
@@ -71,7 +72,14 @@ def get_pathways_proxy_args(args) -> str:
               - --xla_enable_async_all_gather=true
               - --xla_tpu_spmd_rng_bit_generator_unsafe=true
               - --xla_tpu_enable_sunk_dcn_allreduce_done_with_host_reduction=true
-              - --gcs_scratch_location={args.pathways_gcs_location}"""
+              - --gcs_scratch_location={args.pathways_gcs_location}
+              - --xla_sc_disable_megacore_partitioning=true
+              - --xla_tpu_enable_all_reduce_offload_tracing=true
+              - --xla_tpu_use_tc_device_shape_on_sc=true
+              - --xla_tpu_enable_sparse_core_collective_offload_all_reduce=true
+              - --xla_sc_enable_instruction_fusion=false
+              - --xla_sc_disjoint_spmem=false
+              - --deepsea_chip_config_name=megachip_tccontrol"""
 
   if args.use_pathways:
     return yaml.format(args=args, rm_address=get_rm_address(args))
@@ -214,7 +222,8 @@ def get_pathways_rm_args(args, system: SystemCharacteristics) -> str:
               - --node_type=resource_manager
               - --instance_count={instance_count}
               - --temporary_flags_for_debugging=temporary_flag_for_debugging_worker_expected_tpu_chip_config=megachip;;;temporary_flag_for_debugging_megascale_address_derive_from_megascale_grpc=true
-              - --instance_type={instance_type}"""
+              - --instance_type={instance_type}
+              - --deepsea_chip_config_name=megachip_tccontrol"""
   if args.use_pathways:
     return yaml.format(
         args=args,

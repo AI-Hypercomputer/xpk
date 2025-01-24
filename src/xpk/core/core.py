f22463474d934f907de59b8ed2684809887b9713
Piotr Pawłowski <ppawl@google.com>
2025-01-24 16:08:01 +0000

    fix
    Signed-off-by: Piotr Pawłowski <ppawl@google.com>

--- src/xpk/core/core.py
+++ src/xpk/core/core.py
@@ -342,6 +342,7 @@ def setup_k8s_env(args: Namespace) -> k8s_client.ApiClient:
   add_zone_and_project(args)
   get_cluster_credentials(args)
   args.project_number = project_id_to_project_number(args.project)
+
   config.load_kube_config()
   return k8s_client.ApiClient()
 

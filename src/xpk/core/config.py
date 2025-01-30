46633c688fb173bb4781f6e66d289762b53257e9
Piotr Pawłowski <ppawl@google.com>
2025-01-30 13:44:20 +0000

    rm unused print
    Signed-off-by: Piotr Pawłowski <ppawl@google.com>

--- src/xpk/core/config.py
+++ src/xpk/core/config.py
@@ -41,7 +41,6 @@ class XpkConfig:
     self._allowed_keys = default_keys
 
     dir_path = '/'.join(self._config.split('/')[:-1])
-    xpk_print(dir_path)
     if not os.path.exists(dir_path):
       os.makedirs(dir_path)
 

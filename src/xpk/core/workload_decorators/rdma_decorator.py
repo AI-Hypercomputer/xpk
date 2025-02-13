6546aaa1e169535e7463a285043d5130bc0c2ead
Piotr Pawłowski <ppawl@google.com>
2025-02-13 14:23:31 +0000

    fix
    Signed-off-by: Piotr Pawłowski <ppawl@google.com>

--- src/xpk/core/workload_decorators/rdma_decorator.py
+++ src/xpk/core/workload_decorators/rdma_decorator.py
@@ -16,7 +16,7 @@ limitations under the License.
 
 import yaml
 from ...utils.yaml import literal_string
-from ...core.storage import GCS_FUSE_TYPE
+from ...core.storage import GCS_FUSE_TYPE, get_storage_volumes_yaml_dict
 
 
 def decorate_jobset(jobset_manifest_str, sub_networks) -> str:
@@ -65,10 +65,11 @@ def decorate_jobset_with_storages(jobset_manifest_str, storages) -> str:
   """
 
   manifest = yaml.safe_load(jobset_manifest_str)
-
+  storage_volumes = get_storage_volumes_yaml_dict(storages)
   for job in manifest['spec']['replicatedJobs']:
     job_manifest = job['template']
     add_storage_annotations(job_manifest, storages)
+    add_storage_volumes(job_manifest, storage_volumes)
 
   return yaml.dump(manifest, sort_keys=False)
 
@@ -79,6 +80,11 @@ def add_storage_annotations(job_manifest, storages):
   gcs_present = [storage.type == GCS_FUSE_TYPE for storage in storages]
   if gcs_present:
     annotations.update({'gke-gcsfuse/volumes': 'true'})
+
+
+def add_storage_volumes(job_manifest, volumes):
+  volumes = job_manifest['spec']['template']['spec']['volumes']
+  volumes.extend(volumes)
 
 
 def add_annotations(job_manifest, sub_networks):
--- src/xpk/core/workload_decorators/tcpxo_decorator.py
+++ src/xpk/core/workload_decorators/tcpxo_decorator.py
@@ -16,7 +16,8 @@ limitations under the License.
 
 import yaml
 from ...utils.yaml import literal_string
-from ...core.storage import GCS_FUSE_TYPE
+from ...core.storage import GCS_FUSE_TYPE, get_storage_volumes_yaml_dict
+
 # Component version
 rxdm = 'v1.0.12'
 
@@ -68,12 +69,17 @@ def decorate_jobset_with_storages(jobset_manifest_str, storages) -> str:
   """
 
   manifest = yaml.safe_load(jobset_manifest_str)
-
+  storage_volumes = get_storage_volumes_yaml_dict(storages)
   for job in manifest['spec']['replicatedJobs']:
     job_manifest = job['template']
     add_storage_annotations(job_manifest, storages)
-
+    add_storage_volumes(job_manifest, storage_volumes)
   return yaml.dump(manifest, sort_keys=False)
+
+
+def add_storage_volumes(job_manifest, volumes):
+  volumes = job_manifest['spec']['template']['spec']['volumes']
+  volumes.extend(volumes)
 
 
 def add_storage_annotations(job_manifest, storages):

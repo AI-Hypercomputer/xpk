3a0dbe1101a2ab839e5eb22da142c22acb1606fb
Farhad Sharabiani <sharabiani@google.com>
2025-02-20 15:32:07 +0000

    lint fixes

--- src/xpk/commands/cluster_gcluster.py
+++ src/xpk/commands/cluster_gcluster.py
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import os
-
 from ..core.blueprint.blueprint_generator import (
     BlueprintGenerator,
     BlueprintGeneratorOutput,

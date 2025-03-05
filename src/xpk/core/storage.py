80f784093a192671cbefffba13c72894ebf91186
gcie <gciesielski@google.com>
2025-03-05 14:55:12 +0000

    remove unused import os

--- src/xpk/core/storage.py
+++ src/xpk/core/storage.py
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import os
 from argparse import Namespace
 from dataclasses import dataclass
 from importlib.resources import files

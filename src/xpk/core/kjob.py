81b2ee20f1ad07640fc60a37bd5ebc9a1e0c3a0e
Farhad Sharabiani <sharabiani@google.com>
2025-03-13 18:34:19 +0000

    slurm priority support

--- src/xpk/core/kjob.py
+++ src/xpk/core/kjob.py
@@ -78,6 +78,7 @@ job_template_yaml = """
               workingDir: {working_directory}
               {resources}
           {node_selector}
+          priorityClassName: {priority}
           restartPolicy: OnFailure
           serviceAccountName: {service_account}
 """
@@ -240,6 +241,7 @@ def create_job_template_instance(
           working_directory=working_directory,
           resources=resources,
           node_selector=node_selector,
+          priority=args.priority,
           service_account=service_account,
       ),
       task="Creating JobTemplate",

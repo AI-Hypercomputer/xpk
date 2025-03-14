ad83ef6ac4197bcb8b1e74a17c6b5da5ea9a338d
Grzegorz Ciesielski <gciesielski@google.com>
2025-03-14 10:48:56 +0100

    Do not require manifest when attaching storage (#403)
    * drop manifest requirement when attaching gcsfuse storage
    
    * docs
    
    * do not require manifest in fuse workflow tests
    
    * change 'user' to 'you'
    
    * unify gcsfuse template generation algo to match filestore
    
    * gcsfuse storage attach add missing size in docs
    
    * dromp manifest requirement for attaching filestore storage
    
    * remove obsolete `--manifest` param
    
    * fix: linting and typing
    
    * fix: linting
    
    * separate filestore instance name from storage volume names
    
    * fix: typo
    
    * minor fixes
    
    * remove obsolete error messages
    
    * docs
    
    * fix: readme inconsistencies
    
    * fix: add project id to storage manifest file path
    
    * refactor create_storage_crds
    
    * use Availability.ZONAL instead of hardcoded string value
    
    * move loading filestore location logic to load_location function
    
    * fix: fuse test messages
    
    * fix tests: always remove fuse storage after failing tests
    
    * fix error caused by same storage names
    
    * remove oboslete code
    
    * fix links in readme
    
    * refactor FilestoreClient.load_location
    
    * FilestoreClient.load_template
    
    * remove redundant "filestore" in function names
    
    * add default values for access-mode, tier and auto-mount
    
    * remove filestore in case of workflow error
    
    * refactor check_instance to not modify state
    
    * refactor load_template to utils.templates.py
    
    * fix: pytype
    
    * remove unused manifests
    
    * remove filestore storage attach test
    
    it needs a filestore instance now that it does not require adding manifest by hand
    
    * fix: add size param when attaching gscfuse storage in workflows
    
    * fix tests

--- src/xpk/core/filestore.py
+++ src/xpk/core/filestore.py
@@ -14,22 +14,19 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from google.cloud import filestore_v1
-
-from google.cloud.filestore_v1.types import Instance
-from google.cloud.filestore_v1.types import FileShareConfig
-from google.cloud.filestore_v1.types import NetworkConfig
-from google.cloud.exceptions import GoogleCloudError
-
-from ..utils.console import xpk_exit, xpk_print
-from .cluster import zone_to_region
 from enum import Enum
 
-import os
-import ruamel.yaml
+from google.cloud import filestore_v1
+from google.cloud.exceptions import GoogleCloudError
+from google.cloud.filestore_v1.types import (
+    FileShareConfig,
+    Instance,
+    NetworkConfig,
+)
 
-yaml = ruamel.yaml.YAML()
-yaml_object_separator = "---\n"
+from ..utils import templates
+from ..utils.console import xpk_exit, xpk_print
+from .cluster import zone_to_region
 
 FS_PV_PATH = "/../templates/filestore-pv.yaml"
 FS_PVC_PATH = "/../templates/filestore-pvc.yaml"
@@ -65,18 +62,21 @@ def get_pvc_name(storage_name: str) -> str:
 class FilestoreClient:
   """FilestoreClient is a class for interacting with GCP filestore instances."""
 
-  def __init__(self, zone: str, name: str, project: str, tier: str) -> None:
+  def __init__(
+      self,
+      zone: str,
+      name: str,
+      project: str,
+  ) -> None:
     self.zone = zone
     self.region = zone_to_region(zone)
-    self.tier = tier
-    self.availability = TIERS[tier].value
     self.name = name
     self.project = project
     self._client = filestore_v1.CloudFilestoreManagerClient()
+    self.instance: Instance | None = None
 
-  def check_filestore_instance_exists(
-      self,
-  ) -> bool:
+  def get_instance(self) -> Instance | None:
+    """Get existing Filestore instance"""
     parentZonal = self.get_parent(self.zone)
     parentRegional = self.get_parent(self.region)
     reqZonal = filestore_v1.ListInstancesRequest(parent=parentZonal)
@@ -88,23 +88,35 @@ class FilestoreClient:
       xpk_print(f"Exception while trying to list instances {e}")
       xpk_exit(1)
 
-    fullname_zonal = self.get_instance_fullname(parentZonal)
-    fullname_regional = self.get_instance_fullname(parentRegional)
+    fullname_zonal = self.get_instance_fullname(self.zone)
+    fullname_regional = self.get_instance_fullname(self.region)
 
     for instance in instancesZonal:
       if instance.name == fullname_zonal:
-        return True
+        return instance  # pytype: disable=bad-return-type
 
     for instance in instancesRegional:
       if instance.name == fullname_regional:
-        return True
+        return instance  # pytype: disable=bad-return-type
 
-    return False
+  def check_instance_exists(self) -> bool:
+    """Check if Filestore instance exists"""
+    instance = self.get_instance()
+    return instance is not None
 
-  def create_filestore_instance(
+  def load_instance(self) -> None:
+    if self.instance is None:
+      self.instance = self.get_instance()
+
+  def get_instance_location(self) -> str:
+    """Get Filestore instance's location"""
+    return str(self.instance.name.split("/")[3])
+
+  def create_instance(
       self,
       vol: str,
       size: int,
+      tier: str,
       connect_mode=None,
       reserved_ip_range=None,
       network: str = "default",
@@ -115,6 +127,12 @@ class FilestoreClient:
       modes=None,
   ) -> None:
     """Create new Filestore instance"""
+
+    location = (
+        self.zone
+        if TIERS[tier].value == Availability.ZONAL.value
+        else self.region
+    )
 
     file_shares = [
         FileShareConfig(
@@ -133,112 +151,85 @@ class FilestoreClient:
         )
     ]
     request = filestore_v1.CreateInstanceRequest(
-        parent=self.get_parent(),
+        parent=self.get_parent(location),
         instance_id=self.name,
         instance=Instance(
             description=description,
-            tier=self.tier,
+            tier=tier,
             kms_key_name=kms_key_name,
             file_shares=file_shares,
             networks=networks,
         ),
     )
-
     # Make the request
     operation = self._client.create_instance(request=request)
     xpk_print("Waiting for filestore creation to complete...")
-    response = None
+    self.instance = None
     try:
-      response = operation.result()
+      self.instance = operation.result()
     except GoogleCloudError as e:
       xpk_print(f"Error while creating Filestore instance: {e}")
       xpk_exit(1)
-    xpk_print(f"Filestore instance {self.get_parent()} created")
-    self.response = response
+    xpk_print(
+        f"Filestore instance {self.get_instance_fullname(location)} created"
+    )
 
-  def create_sc(self, network: str, project: str) -> dict:
-    abs_path = f"{os.path.dirname(__file__)}{FS_SC_PATH}"
-    with open(abs_path, "r", encoding="utf-8") as file:
-      data: dict = yaml.load(file)
-    data["metadata"]["name"] = get_storage_class_name(self.name)
-    data["parameters"]["tier"] = self.tier
+  def create_sc(self, name: str, network: str) -> dict:
+    """Create a yaml representing filestore StorageClass."""
+    data = templates.load(FS_SC_PATH)
+    data["metadata"]["name"] = get_storage_class_name(name)
+    data["parameters"]["tier"] = self.instance.tier.name
     data["parameters"][
         "network"
-    ] = f"projects/{project}/global/networks/{network}"
+    ] = f"projects/{self.project}/global/networks/{network}"
     return data
 
-  def create_pv(self, vol: str, access_mode: str) -> dict:
-    abs_path = f"{os.path.dirname(__file__)}{FS_PV_PATH}"
-    with open(abs_path, "r", encoding="utf-8") as file:
-      data: dict = yaml.load(file)
-
-    data["metadata"]["name"] = get_pv_name(self.name)
-    spec = data["spec"]
-    spec["storageClassName"] = get_storage_class_name(self.name)
-    spec["capacity"]["storage"] = self.response.file_shares[0].capacity_gb
-    spec["accessModes"] = [access_mode]
-    volumeHandle = f"{self.get_instance_fullname()}/volumes/{vol}"
-    spec["csi"]["volumeHandle"] = volumeHandle
-    spec["csi"]["volumeAttributes"]["ip"] = self.response.networks[
-        0
-    ].ip_addresses[0]
-    spec["csi"]["volumeAttributes"]["volume"] = vol
-    data["spec"] = spec
-    return data
-
-  def create_pvc(self, access_mode: str) -> dict:
-    """Create a yaml representing filestore PV and PVC and save it to file.
-
-    Args:
-        filepath (str): path to which yaml file containing PV and PVC will be saved
-
-    Returns:
-      None
-    """
-    abs_path = f"{os.path.dirname(__file__)}{FS_PVC_PATH}"
-    with open(abs_path, "r", encoding="utf-8") as file:
-      data: dict = yaml.load(file)
-    data["metadata"]["name"] = get_pvc_name(self.name)
-    spec = data["spec"]
-    spec["accessModes"] = [access_mode]
-    spec["storageClassName"] = get_storage_class_name(self.name)
-    spec["volumeName"] = get_pv_name(self.name)
-    spec["resources"]["requests"]["storage"] = self.response.file_shares[
+  def create_pv(self, name: str, vol: str, access_mode: str) -> dict:
+    """Create a yaml representing filestore PersistentVolume."""
+    data = templates.load(FS_PV_PATH)
+    data["metadata"]["name"] = get_pv_name(name)
+    data["spec"]["storageClassName"] = get_storage_class_name(name)
+    data["spec"]["capacity"]["storage"] = self.instance.file_shares[
         0
     ].capacity_gb
-    data["spec"] = spec
+    data["spec"]["accessModes"] = [access_mode]
+    volumeHandle = f"{self.get_instance_fullname()}/volumes/{vol}"
+    data["spec"]["csi"]["volumeHandle"] = volumeHandle
+    data["spec"]["csi"]["volumeAttributes"]["ip"] = self.instance.networks[
+        0
+    ].ip_addresses[0]
+    data["spec"]["csi"]["volumeAttributes"]["volume"] = vol
     return data
 
-  def get_location(self) -> str:
-    """Get gcp location based on the Filestore's tier"""
-    return (
-        self.region
-        if self.availability == Availability.REGIONAL.value
-        else self.zone
+  def create_pvc(self, name: str, access_mode: str) -> dict:
+    """Create a yaml representing filestore PersistentVolumeClaim."""
+    data = templates.load(FS_PVC_PATH)
+    data["metadata"]["name"] = get_pvc_name(name)
+    data["spec"]["accessModes"] = [access_mode]
+    data["spec"]["storageClassName"] = get_storage_class_name(name)
+    data["spec"]["volumeName"] = get_pv_name(name)
+    data["spec"]["resources"]["requests"]["storage"] = (
+        self.instance.file_shares[0].capacity_gb
     )
+    return data
+
+  def manifest(
+      self, name: str, vol: str, access_mode: str, network: str
+  ) -> list[dict]:
+    self.load_instance()
+    pv = self.create_pv(name, vol, access_mode)
+    pvc = self.create_pvc(name, access_mode)
+    sc = self.create_sc(name, network)
+    return [pv, pvc, sc]
 
   def get_parent(self, location: str | None = None) -> str:
-    """Get the Filestore's name's parent"""
+    """Get the Filestore's parent's name"""
     if location is None:
-      location = self.get_location()
+      location = self.get_instance_location()
     return f"projects/{self.project}/locations/{location}"
 
-  def get_instance_fullname(self, parent: str | None = None) -> str:
-    """Get the Filestore's name's parent"""
-    if parent is None:
-      parent = self.get_parent()
-    return f"{parent}/instances/{self.name}"
-
-  def compile_to_manifest_yaml(
-      self, manifests_path: str, sc: dict, pv: dict, pvc: dict
-  ) -> str:
-    manifest_file = (
-        f"{manifests_path}/{self.project}-{self.zone}-{self.name}-manifest.yaml"
-    )
-    with open(manifest_file, mode="w+", encoding="utf-8") as f:
-      yaml.dump(sc, f)
-      f.write(yaml_object_separator)
-      yaml.dump(pv, f)
-      f.write(yaml_object_separator)
-      yaml.dump(pvc, f)
-    return manifest_file
+  def get_instance_fullname(self, location: str | None = None) -> str:
+    """Get the Filestore's full name"""
+    if location is None:
+      location = self.get_instance_location()
+    return f"projects/{self.project}/locations/{location}/instances/{self.name}"

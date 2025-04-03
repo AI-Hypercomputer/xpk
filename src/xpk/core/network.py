c8f23b92b733e24bd79566603e4b45610c2a2b7d
Grzegorz Ciesielski <gciesielski@google.com>
2025-04-03 14:59:56 +0200

    Disable creating additional networks for L4 and A2 clusters (#444)
    * limit number of networks on GPUs < A3 to 1
    
    * disable creation of additional networks for A2/L4

--- src/xpk/core/network.py
+++ src/xpk/core/network.py
@@ -16,10 +16,8 @@ limitations under the License.
 
 from ..utils.console import xpk_print
 from ..utils.file import write_tmp_file
-from .capacity import H100_DEVICE_TYPE
 from .commands import run_command_for_value, run_command_with_updates
 from .gcloud_context import zone_to_region
-from .system_characteristics import SystemCharacteristics
 
 # cluster_network_yaml: the config when creating the network for a3 cluster
 CLUSTER_NETWORK_YAML = """
@@ -247,20 +245,18 @@ def create_cluster_network_config(args) -> int:
   return 0
 
 
-def set_up_cluster_network_for_gpu(args, system: SystemCharacteristics) -> int:
-  """Set up GKE Cluster networks, subnets and firewall rules for A3/A3+.
-  Note: there are 4 NICs for GPU-GPU bw and 1 NIC for host in an A3 node,
-  and there are 8 NICs for GPU-GPU bw and 1 NIC for host in an A3+ node.
+def set_up_cluster_network_for_a3(args) -> int:
+  """Set up GKE Cluster networks, subnets and firewall rules for A3.
+  Note: there are 4 NICs for GPU-GPU bw and 1 NIC for host in an A3 node.
 
   Args:
     args: user provided arguments for running the command.
-    system: system characteristics.
 
   Returns:
     0 if successful and 1 otherwise.
   """
-  num_networks = 5 if system.device_type == H100_DEVICE_TYPE else 9
-  for i in range(1, num_networks):
+  num_networks = 4
+  for i in range(1, num_networks + 1):
     return_code = create_cluster_network(args, i)
     if return_code != 0:
       return 1

d28129ca07d2f289f477f01d19ed46ba52d059ae
Grzegorz Ciesielski <gciesielski@google.com>
2025-04-14 11:55:35 +0200

    Add `--mount-options` parameter to `xpk storage attach/create` (#450)
    * add mount-options param
    
    * set default value for mount-options
    
    * add missing param
    
    * add default value to mount-options for xpk storage create
    
    * documentation

--- src/xpk/parser/storage.py
+++ src/xpk/parser/storage.py
@@ -146,12 +146,18 @@ def add_storage_attach_parser(
 
   opt_args = storage_attach_parser.add_argument_group(
       'Optional Arguments',
-      'Optional arguments for storage create.',
+      'Optional arguments for storage attach.',
   )
   opt_args.add_argument(
       '--manifest',
       type=str,
       help='Path to manifest file containing volume definitions',
+  )
+  opt_args.add_argument(
+      '--mount-options',
+      type=str,
+      help='Comma-separated list of mountOptions for PersistentVolume',
+      default='implicit-dirs',
   )
   add_kind_cluster_arguments(opt_args)
 
@@ -247,6 +253,12 @@ def add_storage_create_parser(
       '--manifest',
       type=str,
       help='Path to manifest file containing volume definitions',
+  )
+  opt_args.add_argument(
+      '--mount-options',
+      type=str,
+      help='Comma-separated list of mountOptions for PersistentVolume',
+      default='',
   )
 
   add_kind_cluster_arguments(opt_args)

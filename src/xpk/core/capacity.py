183f7cf2e7b72f79ff90663323c6bc0c419486b7
gcie <gciesielski@google.com>
2025-07-10 15:01:58 +0000

    fix yaml formatting for workloads with TPU and NAP

--- src/xpk/core/capacity.py
+++ src/xpk/core/capacity.py
@@ -234,7 +234,7 @@ def get_capacity_node_selectors_from_capacity_type(
     case CapacityType.FLEX_START.name:
       node_selector = 'cloud.google.com/gke-queued="true"'
     case CapacityType.SPOT.name:
-      node_selector = 'cloud.google.com/gke-spot="true"'
+      node_selector = 'cloud.google.com/gke-spot: "true"'
     case CapacityType.RESERVATION.name:
       node_selector = f'cloud.google.com/reservation-name: {args.reservation}'
     case _:

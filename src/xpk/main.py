f1cddab74edcfc7a69fe2fff0f33a51fa14ad4cf
Jacky Fang <jacky.fang@cienet.com>
2025-10-02 08:55:37 +0000

    feat: revert the if statement logic

--- src/xpk/main.py
+++ src/xpk/main.py
@@ -67,11 +67,11 @@ def main() -> None:
   main_args = parser.parse_args()
   main_args.enable_ray_cluster = False
   set_dry_run('dry_run' in main_args and main_args.dry_run)
-  if not main_args.dry_run and not main_args.skip_validation:
+  if main_args.dry_run or main_args.skip_validation:
+    xpk_print('Skipping dependency validation.', flush=True)
+  else:
     xpk_print('Validating dependencies...', flush=True)
     validate_dependencies()
-  else:
-    xpk_print('Skipping dependency validation.', flush=True)
   main_args.func(main_args)
   xpk_print('XPK Done.', flush=True)
 

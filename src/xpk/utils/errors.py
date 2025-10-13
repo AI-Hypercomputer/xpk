a96fcbcdfdd9a6e7df5b24eef70086fc604e8b20
Konrad Kaim <konradkaim@google.com>
2025-10-13 14:00:14 +0000

    style: lint

--- src/xpk/utils/errors.py
+++ src/xpk/utils/errors.py
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+
 class CommandExecutionException(Exception):
   """Exception raised for errors in command execution.
 

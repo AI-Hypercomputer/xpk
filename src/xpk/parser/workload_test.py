4f662765f5355799358319654a5dc2c9a6a78567
Konrad Kaim <31181410+scaliby@users.noreply.github.com>
2025-10-28 10:29:30 +0100

    Cover sub-slicing-topology in workload_tests (#741)
    feat: cover sub-slicing-topology in workload_tests

--- src/xpk/parser/workload_test.py
+++ src/xpk/parser/workload_test.py
@@ -15,7 +15,7 @@ limitations under the License.
 """
 
 import argparse
-from xpk.parser.workload import set_workload_create_parser
+from xpk.parser.workload import set_workload_create_parser, set_workload_create_pathways_parser
 from ..utils.feature_flags import FeatureFlags
 import pytest
 
@@ -32,7 +32,7 @@ def test_workload_create_sub_slicing_topology_is_hidden_with_flag_off():
   set_workload_create_parser(parser)
   help_str = parser.format_help()
 
-  assert "--sub-slicing" not in help_str
+  assert "--sub-slicing-topology" not in help_str
 
 
 def test_workload_create_sub_slicing_topology_is_shown_with_flag_on():
@@ -41,7 +41,7 @@ def test_workload_create_sub_slicing_topology_is_shown_with_flag_on():
   set_workload_create_parser(parser)
   help_str = parser.format_help()
 
-  assert "--sub-slicing" in help_str
+  assert "--sub-slicing-topology" in help_str
 
 
 def test_workload_create_sub_slicing_topology_is_none_by_default():
@@ -66,6 +66,63 @@ def test_workload_create_sub_slicing_topology_can_be_set():
   parser = argparse.ArgumentParser()
 
   set_workload_create_parser(parser)
+  args = parser.parse_args([
+      "--cluster",
+      "test-cluster",
+      "--command",
+      "python3",
+      "--workload",
+      "test",
+      "--tpu-type",
+      "test-tpu",
+      "--sub-slicing-topology",
+      "2x2",
+  ])
+
+  assert args.sub_slicing_topology is "2x2"
+
+
+def test_workload_create_pathways_sub_slicing_topology_is_hidden_with_flag_off():
+  FeatureFlags.SUB_SLICING_ENABLED = False
+  parser = argparse.ArgumentParser()
+
+  set_workload_create_pathways_parser(parser)
+  help_str = parser.format_help()
+
+  assert "--sub-slicing-topology" not in help_str
+
+
+def test_workload_create_pathways_sub_slicing_topology_is_shown_with_flag_on():
+  parser = argparse.ArgumentParser()
+
+  set_workload_create_pathways_parser(parser)
+  help_str = parser.format_help()
+
+  assert "--sub-slicing-topology" in help_str
+
+
+def test_workload_create_pathways_sub_slicing_topology_is_none_by_default():
+  parser = argparse.ArgumentParser()
+
+  set_workload_create_pathways_parser(parser)
+  args = parser.parse_args([
+      "--cluster",
+      "test-cluster",
+      "--command",
+      "python3",
+      "--workload",
+      "test",
+      "--tpu-type",
+      "test-tpu",
+  ])
+
+  assert args.sub_slicing_topology is None
+
+
+def test_workload_create_pathways_sub_slicing_topology_can_be_set():
+  parser = argparse.ArgumentParser()
+
+  set_workload_create_pathways_parser(parser)
   args = parser.parse_args([
       "--cluster",
       "test-cluster",

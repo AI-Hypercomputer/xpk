6576e4c98fcd6688b24bd18a4098cf2c9e19aa0c
Dominik Pająk <pajakd@gmail.com>
2025-11-13 08:28:18 +0100

    Update slice/internal/core/slice.go
    Co-authored-by: Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>

--- slice/internal/core/slice.go
+++ slice/internal/core/slice.go
@@ -49,7 +49,7 @@ func SliceName(workloadName string, podSetName kueue.PodSetReference) string {
 }
 
 func IsStale(slice *v1alpha1.Slice) bool {
-	cond := meta.FindStatusCondition(slice.Status.Conditions, string(v1alpha1.SliceStateConditionType))
+	cond := meta.FindStatusCondition(slice.Status.Conditions, v1alpha1.SliceStateConditionType)
 	return cond != nil && cond.Status == metav1.ConditionFalse && time.Since(cond.LastTransitionTime.Time) >= activationTimeout
 }
 

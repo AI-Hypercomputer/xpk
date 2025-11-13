5ff22e9509860bfb1f7e8b0df97feb2089154984
Feidias Ioannidis <feidias@google.com>
2025-11-13 09:01:18 +0000

    small change

--- src/xpk/core/system_characteristics.py
+++ src/xpk/core/system_characteristics.py
@@ -153,9 +153,7 @@ def get_tpu_system_characteristics_map(
         chips_per_vm=chips_per_vm,
         accelerator_type=AcceleratorType.TPU,
         device_type=f'{prefix}-{num_tensorcores}',
-        requires_workload_policy=requires_workload_policy
-        if vms_per_slice > 1
-        else False,
+        requires_workload_policy=requires_workload_policy and vms_per_slice > 1,
         supports_sub_slicing=supports_sub_slicing,
     )
     system_characteristics_map[f'{prefix}-{topology}'] = system

ac6ef68995be3bcf0bcafcd81d241009afc755e0
Konrad Kaim <konradkaim@google.com>
2025-11-27 09:27:54 +0000

    feat: update kueue version

--- tools/install-xpk.sh
+++ tools/install-xpk.sh
@@ -4,7 +4,7 @@ set -e
 
 pip install --upgrade xpk
 
-curl -Lo /usr/local/bin/kubectl-kueue https://github.com/kubernetes-sigs/kueue/releases/download/v0.12.2/kubectl-kueue-linux-amd64
+curl -Lo /usr/local/bin/kubectl-kueue https://github.com/kubernetes-sigs/kueue/releases/download/v0.14.3/kubectl-kueue-linux-amd64
 
 chmod +x /usr/local/bin/kubectl-kueue
 

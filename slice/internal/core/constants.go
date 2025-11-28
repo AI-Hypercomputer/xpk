ac70bfaa863927bf2e422f63e0ffc85eeaf939f8
Dominik Pajak <dominikpajak@google.com>
2025-11-28 10:44:35 +0000

    API rename

--- slice/internal/core/constants.go
+++ slice/internal/core/constants.go
@@ -22,7 +22,7 @@ const (
 	TPUBlockLabel         = "cloud.google.com/gce-topology-block"
 	TPUSubBlockLabel      = "cloud.google.com/gke-tpu-partition-id-4x4x4"
 
-	TPUSliceHealthNodeSelectorKey   = "cloud.google.com/gke-tpu-partition-4x4x4-state"
+	TPUSliceHealthNodeSelectorKey     = "cloud.google.com/gke-tpu-partition-4x4x4-state"
 	TPUSliceHealthNodeSelectorHealthy = "HEALTHY"
 )
 

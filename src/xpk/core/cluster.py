72f415b36cf2a5262a16a4652c61f95d14a2bc42
Feidias Ioannidis <feidias@google.com>
2025-12-17 10:34:01 +0100

    merge fix

--- src/xpk/core/cluster.py
+++ src/xpk/core/cluster.py
@@ -718,8 +718,10 @@ def get_cluster_credentials(args) -> int:
       location=location,
       dns_endpoint=True,
   )
+  if return_code != 0:
+    return return_code
 
-  if return_code != 0 or not _are_credentials_valid():
+  if not _are_credentials_valid():
     xpk_print('Detected error. Retrying without --dns-endpoint flag...')
     return_code = _get_credentials(
         project=args.project,

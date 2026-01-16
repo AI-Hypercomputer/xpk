daac6e38796659ce91697859de4ecba288871019
Dominik Rabij <dominikrabij@google.com>
2026-01-16 15:20:46 +0100

    SUPER_SLICING_ENABLED=True by default
    We want it to by enabled by default in the Monday release.

--- src/xpk/utils/feature_flags.py
+++ src/xpk/utils/feature_flags.py
@@ -34,7 +34,7 @@ class _FeatureFlags:
   SUB_SLICING_ENABLED = _get_boolean_flag("SUB_SLICING_ENABLED", default=False)
   TELEMETRY_ENABLED = _get_boolean_flag("TELEMETRY_ENABLED", default=True)
   SUPER_SLICING_ENABLED = _get_boolean_flag(
-      "SUPER_SLICING_ENABLED", default=False
+      "SUPER_SLICING_ENABLED", default=True
   )
 
 

ac1fb86c8efecba6d3bfdfd09da63a2fee5a73a7
Jakub Skiba <jakubskiba@google.com>
2026-01-19 13:23:52 +0000

    fix merge

--- slice/internal/core/admissioncheck.go
+++ slice/internal/core/admissioncheck.go
@@ -18,7 +18,7 @@ package core
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta1"
+	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta2"
 )
 
 func BaseSSAAdmissionCheck(ac *kueue.AdmissionCheck) *kueue.AdmissionCheck {

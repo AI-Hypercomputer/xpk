bcbe4479a097aff9ebd8ba317baed3f51a3bff49
Dominik Rabij <dominikrabij@google.com>
2026-02-09 15:48:29 +0100

    Clean up SUPER_SLICING_ENABLED FeatureFlag (#1037)
    * Clean up SUPER_SLICING_ENABLED FeatureFlag
    
    * Remove SUPER_SLICING_ENABLED from recipes and fix lint issues

--- src/xpk/core/nodepool.py
+++ src/xpk/core/nodepool.py
@@ -17,7 +17,6 @@ limitations under the License.
 from typing import Iterator, List
 from itertools import cycle
 
-from ..utils.feature_flags import FeatureFlags
 from ..utils.console import ask_for_user_consent, xpk_print
 from .scheduling import get_placement_policy_name, is_placement_policy_supported
 from .capacity import (
@@ -256,7 +255,7 @@ def run_gke_node_pool_create_command(
 
   placement_args = ''
   if is_placement_policy_supported(system):
-    super_slicing = FeatureFlags.SUPER_SLICING_ENABLED and args.super_slicing
+    super_slicing = args.super_slicing
     placement_policy = get_placement_policy_name(
         system,
         super_slicing,

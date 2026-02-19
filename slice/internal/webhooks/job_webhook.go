01ff0852b45e0968e35a94f32dda2f66b16fe82f
Dominik Pajak <dominikpajak@google.com>
2026-02-19 10:02:24 +0000

    Webhook for job

---
+++ slice/internal/webhooks/job_webhook.go
@@ -0,0 +1,66 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package webhooks
+
+import (
+	"context"
+
+	batchv1 "k8s.io/api/batch/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	kueueconstants "sigs.k8s.io/kueue/pkg/controller/constants"
+
+	"tpu-slice-controller/internal/core"
+)
+
+type JobWebhook struct{}
+
+func SetupJobWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(&batchv1.Job{}).
+		WithDefaulter(&JobWebhook{}).
+		Complete()
+}
+
+// +kubebuilder:webhook:path=/mutate-batch-v1-job,mutating=true,failurePolicy=fail,sideEffects=None,groups=batch,resources=jobs,verbs=create,versions=v1,name=mjob.kb.io,admissionReviewVersions=v1
+
+var _ webhook.CustomDefaulter = &JobWebhook{}
+
+func (r *JobWebhook) Default(ctx context.Context, obj runtime.Object) error {
+	job := obj.(*batchv1.Job)
+	log := ctrl.LoggerFrom(ctx).WithName("job-accelerator-gke-webhook")
+	log.V(5).Info("Defaulting Job")
+
+	if job.Labels[kueueconstants.QueueLabel] == "" {
+		log.V(5).Info("Skipping due to missing Kueue Label")
+		return nil
+	}
+
+	if !core.IsRelevantPodTemplateSpec(job.Spec.Template) {
+		log.V(5).Info("Skipping annotating Job due to TPU Annotation or Node Selector misconfigured")
+		return nil
+	}
+	log.V(5).Info("Annotating Job")
+	annotatePodTemplateSpecWithSliceHealth(&job.Spec.Template)
+	err := annotatePodTemplateSpecWithTopology(&job.Spec.Template, job.Spec.Parallelism, job.Name, "job")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ slice/internal/webhooks/webhook_utils.go
@@ -0,0 +1,89 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package webhooks
+
+import (
+	"fmt"
+	"strconv"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/utils/ptr"
+	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta1"
+
+	"tpu-slice-controller/internal/core"
+	"tpu-slice-controller/internal/topology"
+)
+
+func getTPUsRequestedPerPod(spec corev1.PodSpec) int64 {
+	var totalTPUs int64
+	for _, container := range spec.Containers {
+		if tpuQuantity, ok := container.Resources.Limits[core.TPUResourceName]; ok {
+			totalTPUs += tpuQuantity.Value()
+		}
+	}
+	return totalTPUs
+}
+
+func annotatePodTemplateSpecWithSliceHealth(template *corev1.PodTemplateSpec) {
+	// 1. If there is NodeSelector with TPUSliceHealthNodeSelectorKey, we do nothing.
+	if _, ok := template.Spec.NodeSelector[core.TPUSliceHealthNodeSelectorKey]; ok {
+		return
+	}
+
+	// 2. If there is NodeAffinity with TPUSliceHealthNodeSelectorKey, we do nothing.
+	if template.Spec.Affinity != nil &&
+		template.Spec.Affinity.NodeAffinity != nil &&
+		template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution != nil {
+		for _, term := range template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms {
+			for _, req := range term.MatchExpressions {
+				if req.Key == core.TPUSliceHealthNodeSelectorKey {
+					return
+				}
+			}
+		}
+	}
+
+	// 3. If neither of these, we add a NodeAffinity.
+	core.AddNodeAffinity(template, core.TPUSliceHealthNodeSelectorKey, []string{core.TPUSliceHealthNodeSelectorHealthy})
+}
+
+func annotatePodTemplateSpecWithTopology(template *corev1.PodTemplateSpec, parallelism *int32, resourceName string, resourceKind string) error {
+	if template.Annotations == nil {
+		template.Annotations = make(map[string]string)
+	}
+
+	template.Annotations[kueue.PodSetRequiredTopologyAnnotation] = core.TPUBlockLabel
+	template.Annotations[kueue.PodSetSliceRequiredTopologyAnnotation] = core.TPUSubBlockLabel
+
+	pods := ptr.Deref(parallelism, 1)
+
+	sliceSize, err := topology.CalculateSliceSize(
+		template.Annotations[core.TPUSliceTopologyAnnotation],
+		pods,
+	)
+	if err != nil {
+		return err
+	}
+	tpusRequestedPerPod := getTPUsRequestedPerPod(template.Spec)
+	tpusRequestedPerCube := tpusRequestedPerPod * sliceSize
+	if tpusRequestedPerCube != core.TPUsPerCube {
+		return fmt.Errorf("invalid %s %q: configuration results in %d TPUs requested per cube, but must be exactly %d TPUs (full utilization)", resourceKind, resourceName, tpusRequestedPerCube, core.TPUsPerCube)
+	}
+
+	template.Annotations[kueue.PodSetSliceSizeAnnotation] = strconv.FormatInt(sliceSize, 10)
+	return nil
+}

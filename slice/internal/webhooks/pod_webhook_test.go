ae561635ecdfd95e2f09b7dc99be3dad4c56f9c5
Dominik Pajak <dominikpajak@google.com>
2026-03-04 10:06:29 +0000

    Simplify pod webhook test

--- slice/internal/webhooks/pod_webhook_test.go
+++ slice/internal/webhooks/pod_webhook_test.go
@@ -21,12 +21,7 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/client/fake"
-	jobset "sigs.k8s.io/jobset/api/jobset/v1alpha2"
-	kueueconstants "sigs.k8s.io/kueue/pkg/controller/constants"
 
 	slice "tpu-slice-controller/api/v1beta1"
 	"tpu-slice-controller/internal/core"
@@ -36,14 +31,12 @@ import (
 
 func TestPodDefault(t *testing.T) {
 	const (
-		basePodName    = "pod"
-		baseNamespace  = "default"
-		baseJobSetName = "jobset"
+		basePodName   = "pod"
+		baseNamespace = "default"
 	)
 
 	scheme := runtime.NewScheme()
 	_ = corev1.AddToScheme(scheme)
-	_ = jobset.AddToScheme(scheme)
 
 	makeTPUPod := func(name, ns string) *testingpod.PodWrapper {
 		return testingpod.MakePod(name, ns).
@@ -53,36 +46,11 @@ func TestPodDefault(t *testing.T) {
 
 	testCases := map[string]struct {
 		pod     *corev1.Pod
-		jobSet  *jobset.JobSet
 		wantPod *corev1.Pod
 		wantErr error
 	}{
-		"no jobset label": {
-			pod:     makeTPUPod(basePodName, baseNamespace).Obj(),
-			wantPod: makeTPUPod(basePodName, baseNamespace).Obj(),
-		},
-		"jobset not found": {
-			pod:     makeTPUPod(basePodName, baseNamespace).Label(jobset.JobSetNameKey, baseJobSetName).Obj(),
-			wantPod: makeTPUPod(basePodName, baseNamespace).Label(jobset.JobSetNameKey, baseJobSetName).Obj(),
-		},
-		"jobset found, no queue label": {
-			pod: makeTPUPod(basePodName, baseNamespace).Label(jobset.JobSetNameKey, baseJobSetName).NodeSelectorRequirement(corev1.NodeSelectorRequirement{
-				Key:      core.TPUSliceNodeLabel,
-				Operator: corev1.NodeSelectorOpDoesNotExist,
-			}).Obj(),
-			jobSet: &jobset.JobSet{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      baseJobSetName,
-					Namespace: baseNamespace,
-				},
-			},
-			wantPod: makeTPUPod(basePodName, baseNamespace).Label(jobset.JobSetNameKey, baseJobSetName).NodeSelectorRequirement(corev1.NodeSelectorRequirement{
-				Key:      core.TPUSliceNodeLabel,
-				Operator: corev1.NodeSelectorOpDoesNotExist,
-			}).Obj(),
-		},
-		"jobset found, queue label present, anti-affinity removed": {
-			pod: makeTPUPod(basePodName, baseNamespace).Label(jobset.JobSetNameKey, baseJobSetName).NodeSelectorRequirement(corev1.NodeSelectorRequirement{
+		"anti-affinity removed": {
+			pod: makeTPUPod(basePodName, baseNamespace).NodeSelectorRequirement(corev1.NodeSelectorRequirement{
 				Key:      core.TPUSliceNodeLabel,
 				Operator: corev1.NodeSelectorOpDoesNotExist,
 			}).NodeSelectorRequirement(
@@ -91,16 +59,21 @@ func TestPodDefault(t *testing.T) {
 					Operator: corev1.NodeSelectorOpExists,
 				},
 			).Obj(),
-			jobSet: &jobset.JobSet{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      baseJobSetName,
-					Namespace: baseNamespace,
-					Labels: map[string]string{
-						kueueconstants.QueueLabel: "queue",
-					},
+			wantPod: makeTPUPod(basePodName, baseNamespace).NodeSelectorRequirement(
+				corev1.NodeSelectorRequirement{
+					Key:      "other-key",
+					Operator: corev1.NodeSelectorOpExists,
 				},
-			},
-			wantPod: makeTPUPod(basePodName, baseNamespace).Label(jobset.JobSetNameKey, baseJobSetName).NodeSelectorRequirement(
+			).Obj(),
+		},
+		"no anti-affinity": {
+			pod: makeTPUPod(basePodName, baseNamespace).NodeSelectorRequirement(
+				corev1.NodeSelectorRequirement{
+					Key:      "other-key",
+					Operator: corev1.NodeSelectorOpExists,
+				},
+			).Obj(),
+			wantPod: makeTPUPod(basePodName, baseNamespace).NodeSelectorRequirement(
 				corev1.NodeSelectorRequirement{
 					Key:      "other-key",
 					Operator: corev1.NodeSelectorOpExists,
@@ -112,14 +85,7 @@ func TestPodDefault(t *testing.T) {
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
 			ctx := t.Context()
-			var objs []client.Object
-			if tc.jobSet != nil {
-				objs = append(objs, tc.jobSet)
-			}
-			client := fake.NewClientBuilder().WithScheme(scheme).WithObjects(objs...).Build()
-			webhook := &PodWebhook{
-				client: client,
-			}
+			webhook := &PodWebhook{}
 
 			gotErr := webhook.Default(ctx, tc.pod)
 			if diff := cmp.Diff(tc.wantErr, gotErr, utiltesting.EquateErrors); diff != "" {

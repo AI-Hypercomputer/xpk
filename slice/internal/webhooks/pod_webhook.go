403277646591a3de1674422e29a4007fdcdfde81
Dominik Pajak <dominikpajak@google.com>
2026-03-04 10:01:42 +0000

    Remove redundant checks

--- slice/internal/webhooks/pod_webhook.go
+++ slice/internal/webhooks/pod_webhook.go
@@ -19,16 +19,12 @@ package webhooks
 import (
 	"context"
 
-	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
-	jobset "sigs.k8s.io/jobset/api/jobset/v1alpha2"
-	kueueconstants "sigs.k8s.io/kueue/pkg/controller/constants"
 
 	"tpu-slice-controller/internal/features"
 )
@@ -47,8 +43,6 @@ func SetupPodWebhookWithManager(mgr ctrl.Manager) error {
 }
 
 // +kubebuilder:webhook:path=/mutate--v1-pod,mutating=true,failurePolicy=fail,sideEffects=None,groups="",resources=pods,verbs=create,versions=v1,name=mpod.kb.io,admissionReviewVersions=v1,objectSelector="cloud.google.com/gke-tpu-slice-pod=true"
-// +kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch
-// +kubebuilder:rbac:groups=jobset.x-k8s.io,resources=jobsets,verbs=get;list;watch
 
 var _ webhook.CustomDefaulter = &PodWebhook{}
 
@@ -56,36 +50,8 @@ func (r *PodWebhook) Default(ctx context.Context, obj runtime.Object) error {
 	if features.Enabled(features.NodesInSlicesAntiAffinity) {
 		pod := obj.(*corev1.Pod)
 		log := ctrl.LoggerFrom(ctx).WithName("pod-accelerator-gke-webhook")
-		log.V(5).Info("Defaulting Pod", "pod", klog.KObj(pod))
-		var isKueueManaged bool
-
-		// Check if the Pod is part of a Kueue-managed JobSet
-		if jobSetName, ok := pod.Labels[jobset.JobSetNameKey]; ok {
-			var js jobset.JobSet
-			if err := r.client.Get(ctx, client.ObjectKey{Name: jobSetName, Namespace: pod.Namespace}, &js); err == nil {
-				if js.Labels[kueueconstants.QueueLabel] != "" {
-					isKueueManaged = true
-				}
-			}
-		}
-
-		// Check if the Pod is owned by a Kueue-managed Job
-		if !isKueueManaged {
-			owner := metav1.GetControllerOf(pod)
-			if owner != nil && owner.Kind == "Job" {
-				var job batchv1.Job
-				if err := r.client.Get(ctx, client.ObjectKey{Name: owner.Name, Namespace: pod.Namespace}, &job); err == nil {
-					if job.Labels[kueueconstants.QueueLabel] != "" {
-						isKueueManaged = true
-					}
-				}
-			}
-		}
-
-		if isKueueManaged {
-			log.V(5).Info("Removing anti-affinity from pod", "pod", klog.KObj(pod))
-			removeNodeInSliceAntiAffinity(&pod.Spec)
-		}
+		log.V(5).Info("Defaulting pod by removing anti-affinity", "pod", klog.KObj(pod))
+		removeNodeInSliceAntiAffinity(&pod.Spec)
 	}
 	return nil
 }

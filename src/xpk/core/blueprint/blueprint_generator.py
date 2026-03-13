083b11d93723777114e23d834d47336c6f6bca70
Feidias Ioannidis <feidias@google.com>
2026-03-13 14:07:42 +0000

    Update jobset used by GPUs from version 0.7.2 to 0.8.1

--- src/xpk/core/blueprint/blueprint_generator.py
+++ src/xpk/core/blueprint/blueprint_generator.py
@@ -257,7 +257,7 @@ class BlueprintGenerator:
         source="modules/management/kubectl-apply",
         use=["gke_cluster"],
         settings={
-            "jobset": {"install": True, "version": "v0.7.2"},
+            "jobset": {"install": True, "version": "v0.8.1"},
             "apply_manifests": [{
                 "source": f'$(ghpc_stage("{blueprint_name}"))/storage_crd.yaml'
             }],
@@ -634,7 +634,7 @@ class BlueprintGenerator:
         source="modules/management/kubectl-apply",
         use=[cluster_id],
         settings={
-            "jobset": {"install": True, "version": "v0.7.2"},
+            "jobset": {"install": True, "version": "v0.8.1"},
             "apply_manifests": [
                 {"source": nccl_installer_path},
                 {"source": mlgru_disable_path},
@@ -917,7 +917,7 @@ class BlueprintGenerator:
         source="modules/management/kubectl-apply",
         use=[cluster_id],
         settings={
-            "jobset": {"install": True, "version": "v0.7.2"},
+            "jobset": {"install": True, "version": "v0.8.1"},
             "apply_manifests": [
                 {"source": nccl_installer_path},
                 {

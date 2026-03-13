6939b3df002b5914e9a37109627e7d5d4cc179a2
Dominik Rabij <dominikrabij@google.com>
2026-03-13 18:44:38 +0100

    Revert "Reapply "revert: Revert changes made to src/xpk/commands/cluster.py""
    This reverts commit 635e099b3fc3721ac60b35cd50b945afaadbf64b.

--- src/xpk/commands/cluster.py
+++ src/xpk/commands/cluster.py
@@ -38,7 +38,6 @@ from ..core.cluster_private import authorize_private_cluster_access_if_necessary
 from ..core.commands import (
     run_command_for_value,
     run_command_with_updates,
-    run_command_with_updates_retry,
 )
 from ..core.config import VERTEX_TENSORBOARD_FEATURE_FLAG
 from ..core.telemetry import MetricsCollector, MetricsEventMetadataKey
@@ -192,12 +191,6 @@ def cluster_adapt(args) -> None:
   install_kueue_code = _install_kueue(args, system, autoprovisioning_config)
   if install_kueue_code != 0:
     xpk_exit(install_kueue_code)
-
-  if args.super_slicing:
-    install_kueue_slice_controller_code = _install_kueue_slice_controller()
-    if install_kueue_slice_controller_code != 0:
-      xpk_print('Installation of Kueue Slice Controller failed.')
-      xpk_exit(install_kueue_slice_controller_code)
 
   if system.accelerator_type == AcceleratorType.GPU:
     prepare_gpus(system)
@@ -454,12 +447,6 @@ def cluster_create(args) -> None:
   install_kueue_code = _install_kueue(args, system, autoprovisioning_config)
   if install_kueue_code != 0:
     xpk_exit(install_kueue_code)
-
-  if args.super_slicing:
-    install_kueue_slice_controller_code = _install_kueue_slice_controller()
-    if install_kueue_slice_controller_code != 0:
-      xpk_print('Installation of Kueue Slice Controller failed.')
-      xpk_exit(install_kueue_slice_controller_code)
 
   if system.accelerator_type == AcceleratorType.GPU:
     prepare_gpus(system)
@@ -1345,16 +1332,6 @@ def install_storage_csis(args):
     )
     if update_cluster_command_code != 0:
       xpk_exit(update_cluster_command_code)
-
-
-def _install_kueue_slice_controller() -> int:
-  command = (
-      'kubectl apply -f'
-      ' https://raw.githubusercontent.com/AI-Hypercomputer/xpk/refs/heads/slice-main/slice/manifests.yaml'
-  )
-  return run_command_with_updates_retry(
-      command, 'Install Kueue Slice Controller', num_retry_attempts=3
-  )
 
 
 def _install_kueue(

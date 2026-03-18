7cf58b9bfc087a3866c9e1b199d5dbff380dffbe
Dominik Rabij <dominikrabij@google.com>
2026-03-18 14:12:12 +0100

    Merge if statements for parallel_containers

--- src/xpk/commands/workload.py
+++ src/xpk/commands/workload.py
@@ -557,9 +557,7 @@ def workload_create(args) -> None:
     xpk_exit(1)
 
   parallel_containers = workload_system.parallel_containers
-  if not args.use_parallel_containers:
-    parallel_containers = 1
-  if args.use_pathways:
+  if not args.use_parallel_containers or args.use_pathways:
     parallel_containers = 1
 
   # Currently failure policy rules are supported for Pathways workloads. b/408465881

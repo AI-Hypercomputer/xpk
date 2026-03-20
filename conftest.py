6204435b881a85fcf58be44ea2084bcf66221073
Dominik Rabij <dominikrabij@google.com>
2026-03-20 15:24:06 +0100

    Remove conftest.py

--- conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-import pytest
-
-
-@pytest.fixture(autouse=True, scope="session")
-def setup_feature_flags():
-  os.environ["OPTIONAL_NUM_SLICES"] = "true"
-  os.environ["RESERVATIONS_VALIDATION_ENABLED"] = "true"
